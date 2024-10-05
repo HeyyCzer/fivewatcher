90eb005ac2fb9de2546ab15e4140032cd29a2103
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2024-10-05 18:42:20 -0300

    🔖 Bump to v1.0.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 server_only "yes"
 
 author "HeyyCzer (heyyshop.com.br)"
-version "1.0.0"
+version "1.0.3"
 description "Restarts watched resources when they are modified."
 
 server_script "resource/dist/server.js"
